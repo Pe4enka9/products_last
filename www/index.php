82a97d2ca78fbd0d49660ac4f78bbf99835ae854
student <ivan.tixonov000@gmail.com>
2024-10-10 15:10:15 +0700

    second commit

--- www/index.php
+++ www/index.php
@@ -19,63 +19,58 @@ $currentCategories = $_GET['categories'] ?? array('all');
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/main.css">
     <title>Главная</title>
 </head>
 
-<style>
-    table, tr, th, td {
-        padding: 5px;
-        border: 1px solid #000;
-        border-collapse: collapse;
-    }
-</style>
-
 <body>
 
-<a href="/products.php?categories[]=<?= $currentCategories[0] ?>">Каталог</a>
-<h1>Популярные товары</h1>
+<div class="container">
+    <a class="catalog" href="/products.php?categories[]=<?= $currentCategories[0] ?>">Каталог</a>
+    <h1>Популярные товары</h1>
 
-<form method="get">
-    <select name="categories[]">
-        <option value="all" selected>Все</option>
-        <?php foreach ($categories as $category): ?>
+    <form method="get">
+        <select name="categories[]">
+            <option value="all" selected>Все</option>
+            <?php foreach ($categories as $category): ?>
+                <?php foreach ($currentCategories as $currentCategory): ?>
+                    <?php if ($currentCategory == $category['id']): ?>
+                        <option value="<?= $currentCategory ?>" selected><?= $category['name'] ?></option>
+                    <?php else: ?>
+                        <option value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        </select>
+
+        <input type="submit" value="Искать">
+    </form>
+
+    <table>
+        <thead>
+        <tr>
+            <th>Название</th>
+            <th>Описание</th>
+            <th>Категория</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <?php foreach ($products as $product): ?>
             <?php foreach ($currentCategories as $currentCategory): ?>
-                <?php if ($currentCategory == $category['id']): ?>
-                    <option value="<?= $currentCategory ?>" selected><?= $category['name'] ?></option>
-                <?php else: ?>
-                    <option value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
+                <?php if ($product['popular'] && ($currentCategory == $product['category_id'] || $currentCategory === 'all')): ?>
+                    <tr>
+                        <td><?= $product['name'] ?></td>
+                        <td><?= $product['description'] ?></td>
+                        <td><?= $product['category'] ?></td>
+                        <td><a href="/product.php?slug=<?= $product['slug'] ?>">Перейти</a></td>
+                    </tr>
                 <?php endif; ?>
             <?php endforeach; ?>
         <?php endforeach; ?>
-    </select>
-
-    <input type="submit" value="Искать">
-</form>
-
-<table>
-    <thead>
-    <tr>
-        <th>Название</th>
-        <th>Описание</th>
-        <th>Категория</th>
-    </tr>
-    </thead>
-
-    <tbody>
-    <?php foreach ($products as $product): ?>
-        <?php foreach ($currentCategories as $currentCategory): ?>
-            <?php if ($product['popular'] && ($currentCategory == $product['category_id'] || $currentCategory === 'all')): ?>
-                <tr>
-                    <td><?= $product['name'] ?></td>
-                    <td><?= $product['description'] ?></td>
-                    <td><?= $product['category'] ?></td>
-                    <td><a href="/product.php?slug=<?= $product['slug'] ?>">Перейти</a></td>
-                </tr>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+        </tbody>
+    </table>
+</div>
 
 </body>
 </html>
