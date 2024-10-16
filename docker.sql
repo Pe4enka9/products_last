9df3e01260b77be1b1098b370ab30156df1683e1
Pe4enka9 <158248252+Pe4enka9@users.noreply.github.com>
2024-10-17 00:01:19 +0700

    database

--- docker.sql
+++ docker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: MySQL-8.2
--- Время создания: Окт 11 2024 г., 18:11
+-- Время создания: Окт 17 2024 г., 00:01
 -- Версия сервера: 8.2.0
 -- Версия PHP: 8.3.6
 
@@ -57,19 +57,21 @@ CREATE TABLE `products` (
   `popular` tinyint(1) NOT NULL DEFAULT '0',
   `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `date` datetime NOT NULL,
-  `views` int NOT NULL DEFAULT '0'
+  `views` int NOT NULL DEFAULT '0',
+  `image` varchar(255) DEFAULT NULL,
+  `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Дамп данных таблицы `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `description`, `category_id`, `popular`, `slug`, `date`, `views`) VALUES
-(18, 'Молоко деревенское', '10%', 7, 0, 'moloko-derevenskoe', '2024-10-09 00:00:00', 4),
-(28, 'Вязанка', 'Колбаса вареная', 6, 1, 'vyazanka', '2024-10-10 18:06:27', 38),
-(29, 'Хлеб', 'Ржаной', 10, 1, 'hleb', '2024-10-10 19:23:45', 1),
-(30, 'Аленка', 'Молочный шоколад', 9, 1, 'alenka', '2024-10-10 19:24:55', 10),
-(31, 'Батон', 'Очень вкусный батон', 10, 0, 'baton', '2024-10-11 17:38:41', 1);
+INSERT INTO `products` (`id`, `name`, `description`, `category_id`, `popular`, `slug`, `date`, `views`, `image`, `price`) VALUES
+(18, 'Молоко деревенское', '10%', 7, 0, 'moloko-derevenskoe', '2024-10-09 00:00:00', 4, '/assets/images/milk_dr.jpg', 79.99),
+(28, 'Вязанка', 'Колбаса вареная', 6, 1, 'vyazanka', '2024-10-10 18:06:27', 39, NULL, 299.99),
+(29, 'Хлеб', 'Ржаной', 10, 1, 'hleb', '2024-10-10 19:23:45', 1, '/assets/images/hleb_rn.jpg', 32.99),
+(30, 'Аленка', 'Молочный шоколад', 9, 1, 'alenka', '2024-10-10 19:24:55', 10, '/assets/images/alenka.jpg', 109.99),
+(31, 'Батон', 'Очень вкусный батон', 10, 0, 'baton', '2024-10-11 17:38:41', 11, '/assets/images/baton.jpg', 35.99);
 
 --
 -- Индексы сохранённых таблиц
